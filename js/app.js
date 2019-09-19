442e383a35f6eab83ea18e42590e0fbbe1603a2d
wozzy84 <piotrw84@gmail.com>
2019-09-19 12:46:11 +0200

    commit

---
+++ js/app.js
@@ -0,0 +1,227 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    function setPhoto(index, object) {
+        
+        $(".photo").remove();
+        for (k = 0; k < object.photos.length; k++) {
+            $(".sec_second").append(`<div class="photo">
+                    <img class="img_small photo_${k}" src="${object.photos[index+k].img_src}" class="rounded mx-auto d-block" alt="..." data-camera="${object.photos[index+k].camera.full_name}"
+                    data-id="${object.photos[index+k].id}"
+                    data-earthDate="${object.photos[index+k].earth_date}"></div>`)
+
+        }
+        $('.sec_second').children().hide()
+        $('.sec_second').children().slice(0, 6).show();
+
+    }
+    $.ajax({
+        url: 'https://images-api.nasa.gov/asset/as11-40-5874',
+        dataType: 'json',
+        method: 'GET',
+    }).done(function (response) {
+            console.log(response)
+        $(".sec_first_continer").css("backgroundImage", `url(${response.collection.items[2].href})`)
+    }).fail(function (error) {
+        console.log(error.responseText);
+    })
+
+
+
+    function pagination (index, object) {
+        $(".first_page").text(`${index/6}`)
+        if ($(".first_page").text() == '0') {
+            $(".first_page").css('opacity', '0')
+            $('window').focus();
+        } else {
+            $(".first_page").css('opacity', '1')
+        }
+        $(".second_page").text(`${index/6+1}`)
+        $(".third_page").text(`${index/6+2}`)
+
+        if (object != null) {
+        if (index >= (object.photos.length - object.photos.length % 6-6)){
+            $(".third_page").text("");
+        } 
+    }
+    }
+    pagination(0);
+
+    $(".first-list").hide();
+    $(".second-list").hide();
+
+    // RSvcKCmoFW3dPRfkvReqq8bqL69Q78uAy1XqYh1l
+    function diplayPhotos(rover, sol) {
+        $.ajax({
+            url: `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=RSvcKCmoFW3dPRfkvReqq8bqL69Q78uAy1XqYh1l`,
+            dataType: 'json',
+            method: 'GET',
+        }).done(function (response) {
+
+            let i = 0;
+           
+            setPhoto(i, response);
+            pagination(i, response)
+            if (response.photos.length > 0) {
+                $(".res_name").text(rover)
+                $(".res_sol").text(sol)
+                $(".res_day").text(response.photos[0].earth_date)
+                $(".res_pic").text(response.photos.length)
+            } else {
+                $(".res_name").text(rover)
+                $(".res_sol").text(sol)
+                $(".res_day").text()
+                $(".res_pic").text("Tego dnia nie wykonano zdjęć")
+            }
+
+            $(".next").on('click', function (e) {
+
+                e.preventDefault();
+                
+              
+                if (i < (response.photos.length - response.photos.length % 6-6) && response.photos.length > 6) {
+                    i += 6;
+                    console.log(i)
+                    console.log(response.photos.length)
+                  
+                    $('.sec_second').children().hide()
+                    $('.sec_second').children().slice(i, i + 6).show();
+                    pagination(i, response);
+                }
+            })
+
+            $(".prev").on('click', function (e) {
+                e.preventDefault();
+                if (i >= 6) {
+                    i -= 6;
+                    $('.sec_second').children().hide();
+                    $('.sec_second').children().slice(i, i + 6).show();
+                    pagination(i, response);
+                }
+            })
+
+            $(".first_page").on('click', function (e) {
+                
+                e.preventDefault();
+                if (i >= 6) {
+                    i -= 6;
+                    $('.sec_second').children().hide();
+                    $('.sec_second').children().slice(i, i + 6).show();
+                    pagination(i, response);
+                }
+            })
+
+            $(".second_page").on('click', function (e) {
+                e.preventDefault()
+            })
+            $(".third_page").on('click', function (e) {
+                e.preventDefault();
+                if (i < response.photos.length - 6) {
+                    i += 6;
+                    $('.sec_second').children().hide()
+                    $('.sec_second').children().slice(i, i + 6).show();
+                    pagination(i, response);
+                }
+            })
+ 
+    
+        }).fail(function (error) {
+            console.log(error.responseText);
+        })
+    }
+
+
+    // diplayPhotos();
+
+
+
+    $('.dropdown-item').on('click', function (e) {
+        e.preventDefault();
+        console.log("klik")
+        $(this).closest('.dropdown').find('a.btn').text($(this).text());
+        let rover = $(this).text();
+        $(".first-list").slideDown();
+        $(".second_header").addClass("search_ribbon")
+
+        $.ajax({
+            url: `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?&api_key=RSvcKCmoFW3dPRfkvReqq8bqL69Q78uAy1XqYh1l`,
+            dataType: 'json',
+            method: 'GET',
+        }).done(function (response) {
+            console.log(response)
+            $('.missionName').text(response.photo_manifest.name)
+            $(".launchingDate").text(response.photo_manifest.launch_date)
+            $(".landinngDate").text(response.photo_manifest.landing_date)
+            $(".status").text(response.photo_manifest.status)
+            $(".maxDate").text(response.photo_manifest.max_date)
+            $(".photosTaken").text(response.photo_manifest.total_photos)
+            $(".sols").text(response.photo_manifest.max_sol);
+            $(".pickNumber").attr("placeholder", `Wybierz numer (0 - ${response.photo_manifest.max_sol})`)
+
+        }).fail(function (error) {
+            console.log(error.responseText);
+        })
+
+    })
+
+    $(".accept").on("click", function (e) {
+        e.preventDefault();
+        let roverName = $('.missionName').text();
+        let solNumber = $('.pickNumber').val();
+        diplayPhotos(roverName, solNumber);
+        pagination(0);
+        $('.pickNumber').val('');
+        $('.second-list').slideDown();
+
+        
+    })
+
+    $("body").on("click", ".photo", function () {
+        console.log("klik")
+        console.log($(this))
+        console.log($(this).children()[0].currentSrc)
+        $("body").append('<div class="lightbox"></div>')
+        $("body").css('overflow-x', 'hidden');
+        $("body").css('overflow-y', 'hidden')
+        $("body").append(`<div class="imgBox">
+        <p class="pic_descr"> Mission: ${$(".res_name").text()} | Photo ID : ${$(this).children().data("id")} | Earth date ${$(this).children().data("earthdate")} | Camera: ${$(this).children().data("camera")} </p>
+        <img  class="img_big" src = "">
+        </img>
+        <button type="button" class="close" id="cls" aria-label="Close">Zamknij
+        <span aria-hidden="true">&times;</span>
+      </button>
+        </div>`);
+        $(".imgBox").find('.img_big').attr("src", $(this).children()[0].currentSrc);
+
+    })
+
+    function closeLbox() {
+        $('.img_big').remove();
+        $(".imgBox").remove();
+        $('.lightbox').remove();
+        $("body").css('overflow-x', 'auto');
+        $("body").css('overflow-y', 'auto')
+
+    }
+    $("body").on("click", ".lightbox", function () {
+        closeLbox()
+
+    })
+
+    $("body").on("click", "#cls", function () {
+        closeLbox()
+    })
+
+
+    $(".arrow").click(function(e) {
+        
+        var offset = 20; //Offset of 20px
+    
+        $('html, body').animate({
+            scrollTop: $(".info_sec").offset().top + offset
+        }, 500);
+    });
+
+
+
+
+});
